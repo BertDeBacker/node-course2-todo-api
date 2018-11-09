ca5cf0864a3db33e3dff251c7f1ac388838ef468
Bert De Backer <BertDeBackerkonikx@gmail.com>
2018-11-09 15:58:58 +0100

    add /GET /todos:/id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 
 //local imports
 //use destructuring to create and obtain the variable 'mongoose'
+var { ObjectID } = require('mongodb').ObjectID
 var { mongoose } = require('./db/mongoose')
 var { Todo } = require('./models/todo')
 var { User } = require('./models/user')
@@ -31,6 +32,34 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e)
     })
+})
+
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id
+
+    //validate id using isValid
+    //404 - send back empty send
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Invalid id')
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.status(200).send({ todo })
+        } else {
+            res.status(404).send({})
+        }
+    }, (e) => {
+        res.status(404).send(e)
+    })
+
+    //findById
+    //Success  // if todo - send it back
+    // if not todo - send 404 with empty body
+
+    //Error - 400 - send back nothing
+
 })
 
 app.listen(3001, () => {
