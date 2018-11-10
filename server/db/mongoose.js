033b18cc00f227358ce5f4ee8d71c645f0eaafdf
Bert De Backer <BertDeBackerkonikx@gmail.com>
2018-11-10 12:49:58 +0100

    change mongoose connect settings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
+    //var url = 'mongodb://<dbuser>:<dbpassword>@ds157843.mlab.com:57843/heroku_n25q9576'
+    //var url = 'mongodb://todos:sterren75@ds157843.mlab.com:57843/heroku_n25q9576' 
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 
 module.exports = { mongoose }
